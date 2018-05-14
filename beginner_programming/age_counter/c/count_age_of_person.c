a1ea922d1fd214903cad7e55393ed7d02329fd01
bbbeep <37920434+bbbeep@users.noreply.github.com>
2018-05-14 20:44:01 +0900

    Simple editing--spacing and capitalization
    Not a must but names of variables usually start with small letters, instead of capitals.:)

--- beginner_programming/age_counter/c/count_age_of_person.c
+++ beginner_programming/age_counter/c/count_age_of_person.c
@@ -2,27 +2,27 @@
 
 int main()
 {
-	int BirthMonth, BirthYear;
-	int CurrentMonth, CurrentYear;
-	int AgeYear, AgeMonth;
+	int birthMonth, birthYear;
+	int currentMonth, currentYear;
+	int ageYear, ageMonth;
 	
 	printf("\n\n\t\t\tCount the age person\n\n");
 	printf("Enter Your Birth Year(Eg:1989):");
-	scanf("%d",&BirthYear);
+	scanf("%d",&birthYear);
 	
 	printf("\n\nEnter Your Birth Month(Eg:7):");
-	scanf("%d",&BirthMonth);
+	scanf("%d",&birthMonth);
 	
 	printf("\nEnter The Current Month(Eg:7):");
-	scanf("%d",&CurrentMonth);
+	scanf("%d",&currentMonth);
 	
 	printf("\nEnter The Current Year(Eg:2010):");
-	scanf("%d",&CurrentYear);
+	scanf("%d",&currentYear);
 	
-	AgeYear=CurrentYear-BirthYear;
-	AgeMonth=CurrentMonth-BirthMonth;
+	ageYear = currentYear - birthYear;
+	ageMonth = currentMonth - birthMonth;
 	
-	printf("\n\n\t\tYour Age is %d Years And %d Months ",AgeYear,AgeMonth);
+	printf("\n\n\t\tYour Age is %d Years And %d Months ", ageYear, ageMonth);
 	
 	return 0;
 }
